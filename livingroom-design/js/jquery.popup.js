cc663a432a02c410da1e442cdf35427419920536
purepracticestudio <lilayanashi@gmail.com>
2026-01-21 12:05:14 +0800

    客廳網頁上傳

---
+++ livingroom-design/js/jquery.popup.js
@@ -0,0 +1,102 @@
+﻿//小坪數 
+
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg").hide();
+  //點選了解
+
+  $("#click").click(function(){
+    //淡入顯示背景與對話框
+      $("#black_bg").fadeIn(300);
+  });
+  //點X 回畫面
+  $("#live").click(function(){
+      $("#black_bg").fadeOut(300);
+});
+});
+
+
+
+//小坪數 底部回去看更多設計
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg").hide();
+  //點選了解
+
+  $("#click").click(function(){
+    //淡入顯示背景與對話框
+      $("#black_bg").fadeIn(300);
+  });
+  //點X 回畫面
+  $("#live_right").click(function(){
+      $("#black_bg").fadeOut(300);
+});
+});
+
+
+//小坪數 底部回去看小坪數
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg").hide();
+  //點X 回畫面
+  $("#live_lookbig").click(function(){
+      $("#black_bg").fadeOut(300);
+      $("#black_bg_2").fadeIn(300);
+ 
+});
+});
+
+
+
+
+
+
+
+
+
+//大坪數 
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg_2").hide();
+  //點選了解
+
+  $("#click_2").click(function(){
+    //淡入顯示背景與對話框
+      $("#black_bg_2").fadeIn(300);
+  });
+  //點X 回畫面
+  $("#live_2").click(function(){
+      $("#black_bg_2").fadeOut(300);
+});
+});
+
+//大坪數 底部回去看更多設計
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg_2").hide();
+  //點選了解
+
+  $("#click_2").click(function(){
+    //淡入顯示背景與對話框
+      $("#black_bg_2").fadeIn(300);
+  });
+  //點X 回畫面
+  $("#live_left").click(function(){
+      $("#black_bg_2").fadeOut(300);
+});
+});
+
+
+//大坪數 底部回去看小坪數
+$(function(){
+  //隱藏背景與訊息對話框
+  $("#black_bg_2").hide();
+  //點X 回畫面
+  $("#live_looksmail").click(function(){
+      $("#black_bg_2").fadeOut(300);
+      $("#black_bg").fadeIn(300);
+ 
+});
+});
+
+
---
+++ livingroom-design/js/main.js
@@ -0,0 +1,266 @@
+
+  // 表單
+//縣市下拉式選單
+
+  $(function() {
+    var option = '<option value="0">請選擇縣市</option>';
+    for( var key in CityCounty.cities ){
+        option += '<option value="' + CityCounty.cities[key]['Id'] + ',' + CityCounty.cities[key]['Name'] + '">' + CityCounty.cities[key]['Name'] + '</option>';
+    }
+    $('#city').html(option);
+
+    $('#city').change(function(){
+        switchSiteSearchFormArea($(this).val());
+    });
+  });
+
+  function switchSiteSearchFormArea(flag) {
+    var option = '<option value="0">行政區</option>';
+    var temp = flag.split(",");
+    var cityID = temp[0];
+    for( var key in CityCounty.cities ){
+      if( CityCounty.cities[key]['Id'] == cityID ){
+        for( var i in CityCounty.cities[key].Counties ){
+            option += '<option value="' + CityCounty.cities[key].Counties[i]['Id'] + '">' + CityCounty.cities[key].Counties[i]['Name'] + '</option>';
+        }
+      }
+    }
+    $('#county').html(option);
+    $('#county').hide().show();
+  }
+  // 資料送出
+  function isNumber(str){
+    var chk_result = true;
+
+    for (var i = 0; i < str.length; i++){
+        if (str.charAt(i) < "0" || str.charAt(i) > "9")
+        {
+            chk_result = false;
+            break;
+        }
+    }
+
+    return chk_result;
+  }
+
+  function trim(){
+    if (!this) return this;
+  
+    // 濾掉字串前後的空白
+    for (var begin = 0; begin < this.length; begin++) {
+      if (this.charAt(begin) != " ") break;
+    }
+    for (var end = this.length; end > 0; end--) {
+      if (this.charAt(end - 1) != " ") break;
+    }
+  
+    return this.slice(begin, end);
+  }
+  
+  /**
+  * 檢查欄位是否只包含空白字元
+  * 此函式將在稍後宣告為 String Object's prototype method
+  */
+  function isEmpty()
+  {
+    if (!this) return true;
+  
+    for (var len = 0; len < this.length; len++) {
+      if (this.charAt(len) != " ") return false;
+    }
+  
+    return true;
+  }
+  
+  // 將以上 trim 及 isEmpty 兩函式宣告為 String 的屬性
+  String.prototype.trim = trim;
+  String.prototype.isEmpty = isEmpty;
+
+
+  function chkData(){
+    var nameTrim = $('#name').val().trim();
+    var mobileTrim = $('#mobile').val().trim();
+    $('#name').val(nameTrim);
+    $('#mobile').val(mobileTrim);
+    if($("select[name='county']").val() == null || $("select[name='county']").val() == '0'){
+      alert("【施作區域】欄位，為必填欄位！");
+      return false;
+    }
+  
+    if ($('#name').val().isEmpty()) {
+      alert("【姓名】欄位，為必填欄位！");
+      $('#name').focus();
+      return false;
+    }
+    if ($('#mobile').val().isEmpty()) {
+      alert("【電話】或【市內電話】欄位，未填或格式不正確。");
+      $('#mobile').focus();
+      return false;
+    }
+    if ('' != $('#mobile').val()) {
+        if ( $('#mobile').val().length < 10) {
+            alert("【電話】或【市內電話】欄位，未填或格式不正確。");
+            $('#mobile').focus();
+            return false;
+        }
+        if (!isNumber($('#mobile').val())) {
+            alert("【電話】或【市內電話】欄位，未填或格式不正確。");
+            $('#mobile').select();
+            $('#mobile').focus();
+            return false;
+        }
+    }
+    if ($('#privacyBox:checkbox:checked').length <= 0) {
+      alert("請確認您已閱讀並同意本站【服務條款】，謝謝！");
+      return false;
+    }   
+    
+    return true;
+  }
+  $('.submit').on('click',function(e){
+    e.preventDefault();
+    if(!chkData()) return false;
+    var data = JSON.stringify({ 'CustomerName': $(':text[name="name"]').val(),
+            'CustomerGender': '1',
+            'CustomerMobile': $(':text[name="mobile"]').val(),
+            'CustomerPhone1': '',
+            'CustomerPhone2': '',
+            'CustomerEmail': '',
+            'ContactEarly': '', 'ContactLate': '',
+            'IntroName': '',
+            'IntroDepName': '',
+            'PostalCode': $('select[name="county"]').val(),
+            'HouseStatus': '4',
+            'Note': '客廳知識頁專案來案，請與客戶聯繫後更新資料。', //'專案案來案，請與客戶聯繫後更新資料。'
+            'ServiceItems': [],
+            'TitleName':'客廳這樣設計太舒適！打造待客、起居的完美小天地｜信義居家', //專案的title
+            'TitleUrl': 'https://living.sinyi.com.tw/events/livingroom-design/'   //專案的網址命名
+    });
+    
+    $("#submit").text('送出中...').attr('disabled',true);
+
+    $.ajax({
+      url: '/ajax_frm/submit',
+      type:'POST',
+      data: { 'msg' : data },
+      dataType:'json',
+      success: function(data) {
+         if( data != null ){
+            if( data.result == 0 ){                     
+               alert('表單送出成功！');                    
+               window.location='/frm/finish/' + data.error ;
+            }else{
+               alert( data.error );
+            }
+         }
+      },
+      error: function(data) {
+         alert('web service connection failed!');
+         $("#submit").text('確認送出 ▶').attr('disabled',false);
+         console.log(data);
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+  /*---------- */
+
+  // open mobile menu
+$('.js-toggle-menu').click(function(e){
+  e.preventDefault();
+  $('.mobile-header-nav').slideToggle();
+  $(this).toggleClass('open');
+});
+
+
+$(".js-toggle-menu").on("click", function(e){
+    $(".mobile-header-nav").show();
+
+    $(document).one("click", function(){
+        $(".mobile-header-nav").hide();
+    });
+
+    e.stopPropagation();
+});
+
+
+
+
+
+
+//-BACK TO TOP--//
+
+
+
+// create the back to top button
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 400;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
+
+//-BACK TO TOP--//
+
+
+
+
+  
+
+$(window).load(function(){
+  $('.preloader').fadeOut(1000); // set duration in brackets    
+  $('.menu').addClass("vhide");
+});
+
+
+
+//滾動
+
+$("#NAVNAV1").on("click", function(e){
+  $('html, body').animate({
+    scrollTop: $("#choosepings").offset().top  // 只需修改此處
+  }, 750);  // 750是滑動的時間，單位為毫秒
+  e.preventDefault();
+});
+
+$("#NAVNAV2").on("click", function(e){
+  $('html, body').animate({
+    scrollTop: $("#point").offset().top  // 只需修改此處
+  }, 750);  // 750是滑動的時間，單位為毫秒
+  e.preventDefault();
+});
+
+
+$("#NAVNAV3").on("click", function(e){
+  $('html, body').animate({
+    scrollTop: $("#recommend").offset().top  // 只需修改此處
+  }, 750);  // 750是滑動的時間，單位為毫秒
+  e.preventDefault();
+});
+
+
+$("#NAVNAV4").on("click", function(e){
+  $('html, body').animate({
+    scrollTop: $("#article").offset().top  // 只需修改此處
+  }, 750);  // 750是滑動的時間，單位為毫秒
+  e.preventDefault();
+});
